25cfc765b3bb2f8031a0274e4e8c35d77ad497d1
Caitlin2694 <21324675@student.uwa.edu.au>
2024-03-09 08:27:53 +0800

    initial commit

---
+++ RunWidoco.py
@@ -0,0 +1,31 @@
+import subprocess
+import os
+
+def run_widoco(ontology_filename, ontology_shortname):
+
+    command = """
+        java -jar widoco-1.4.20-jar-with-dependencies_JDK-17.jar \\
+        -ontFile output/"""+ontology_filename+"""\\
+        -outFolder output/widoco/"""+ontology_shortname+""" \\
+        -confFile widoco_conf/"""+ontology_shortname+"""\\
+        -rewriteAll \\
+        -saveConfig output/widoco/"""+ontology_shortname+"""/config \\
+        -htaccess \\
+        -oops \\
+        -ignoreIndividuals \\
+        -includeAnnotationProperties \\
+        -displayDirectImportsOnly \\
+        -excludeIntroduction \\
+        -uniteSections \\
+        -noPlaceHolderText"""
+
+    print(command)
+
+
+    result = subprocess.run(command
+        , stdout=subprocess.PIPE
+        , stderr=subprocess.PIPE
+        , shell=True
+        , cwd="generator/config")
+        
+    print(result.stderr.decode("utf-8"))
